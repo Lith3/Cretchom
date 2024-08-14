c4816c32bd46f086ba86e7822f885f82d59b0c91
JorisB <jorisbudin@gmail.com>
2024-08-14 15:51:19 +0200

    add faker for the seeder

--- server/database/fixtures/HomeStructureSeeder.js
+++ server/database/fixtures/HomeStructureSeeder.js
@@ -13,40 +13,40 @@ class HomeStructureSeeder extends AbstractSeeder {
     });
   }
 
-  run() {
-    const homeStructure = [
-      {
-        postal_code: 69002,
-        capacity: 25,
-        is_professional: true,
-        cat: true,
-        dog: true,
-        price: 25,
-        user_id: 1,
-      },
-      {
-        postal_code: 69007,
-        capacity: 4,
-        is_professional: false,
-        cat: true,
-        dog: false,
-        price: 18,
-        user_id: 2,
-      },
-      {
-        postal_code: 69003,
-        capacity: 3,
-        is_professional: false,
-        cat: false,
-        dog: true,
-        price: 18,
-        user_id: 3,
-      },
-    ];
+  static generateUniqueId() {
+    const generatedIds = new Set();
+    return function userId() {
+      let id;
+      do {
+        id = Math.floor(Math.random() * 100); // Génère un nombre aléatoire jusqu'à 100
+      } while (generatedIds.has(id));
+      generatedIds.add(id);
+      return id;
+    };
+  }
 
-    homeStructure.forEach((structure) => {
-      this.insert(structure);
-    });
+  async run() {
+    const insertPromises = [];
+    const getUniqueId = HomeStructureSeeder.generateUniqueId();
+
+    for (let i = 0; i < 50; i += 1) {
+      const uniqueId = getUniqueId();
+
+      const fakeHomeStructure = {
+        postal_code: this.faker.number.int({ min: 69001, max: 69999 }),
+        capacity: this.faker.number.int({ min: 1, max: 50 }),
+        is_professional: this.faker.datatype.boolean({ probability: 0.4 }),
+        cat: this.faker.datatype.boolean({ probability: 0.6 }),
+        dog: this.faker.datatype.boolean({ probability: 0.6 }),
+        price: this.faker.number.int({ min: 1, max: 50 }),
+        user_id: uniqueId, // Utilisez l'ID unique généré
+        refName: `home_structure_${uniqueId}`,
+      };
+
+      insertPromises.push(this.insert(fakeHomeStructure));
+    }
+
+    await Promise.all(insertPromises);
   }
 }
 
