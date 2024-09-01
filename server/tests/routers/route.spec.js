2bfeb52296013276f1a7077de40b02e45b682741
JorisB <jorisbudin@gmail.com>
2024-09-01 18:01:06 +0200

    fix some spelling mistakes and css bugs

--- server/tests/routers/route.spec.js
+++ server/tests/routers/route.spec.js
@@ -1,13 +1,7 @@
-const { app, request, database } = require("../config");
+const { app, request } = require("../config");
 // test for visistor
 describe("Routes", () => {
   it("should return 401 for /user/:id route", async () => {
-    // Mock empty rows returned from the database
-    const rows = [];
-
-    // Mock the implementation of the database query method
-    jest.spyOn(database, "query").mockImplementation(() => [rows]);
-
     const res = await request(app).get("/api/user/1");
     expect(res.statusCode).toBe(401);
   });
